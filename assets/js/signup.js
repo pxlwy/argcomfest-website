9d66bb3c5e64082bc37326ea11ac4e6ac41a5a2b
Pax Lowey <mxpxlwy@gmail.com>
2023-02-03 02:54:03 +0000

    initial coming soon commit

---
+++ assets/js/signup.js
@@ -0,0 +1,96 @@
+
+    $(document).ready(function(){
+      // Vars.
+				var $form = document.querySelectorAll('#signup-form')[0],
+					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+					$message;
+
+			// Bail if addEventListener isn't supported.
+				if (!('addEventListener' in $form))
+					return;
+
+			// Message.
+				$message = document.createElement('span');
+					$message.classList.add('message');
+					$form.appendChild($message);
+
+				$message._show = function(type, text) {
+
+					$message.innerHTML = text;
+          $message.classList.remove('success', 'failure', 'working');
+					$message.classList.add(type);
+					$message.classList.add('visible');
+
+					window.setTimeout(function() {
+						$message._hide();
+					}, 3000);
+
+				};
+
+				$message._hide = function() {
+					$message.classList.remove('visible');
+				};
+
+        ajaxMailChimpForm($("#signup-form"), $("#subscribe-result"));
+        // Turn the given MailChimp form into an ajax version of it.
+        // If resultElement is given, the subscribe result is set as html to
+        // that element.
+        function ajaxMailChimpForm($form, $resultElement){
+            // Hijack the submission. We'll submit the form manually.
+            $form.submit(function(e) {
+                e.preventDefault();
+                $submit.disabled = true;
+                $message._hide();
+                if (!isValidEmail($form)) {
+                  $message._show('failure', 'Invalid email');
+                  $submit.disabled = false;
+                } else {
+                    $message._show('working', 'Subscribing…');
+                    submitSubscribeForm($form, $resultElement);
+                }
+            });
+        }
+        // Validate the email address in the form
+        function isValidEmail($form) {
+            // If email is empty, show error message.
+            // contains just one @
+            var email = $form.find("input[type='email']").val();
+            if (!email || !email.length) {
+                return false;
+            } else if (email.indexOf("@") == -1) {
+                return false;
+            }
+            return true;
+        }
+        // Submit the form with an ajax/jsonp request.
+        // Based on http://stackoverflow.com/a/15120409/215821
+        function submitSubscribeForm($form, $resultElement) {
+            $.ajax({
+                type: "GET",
+                url: $form.attr("action"),
+                data: $form.serialize(),
+                cache: false,
+                dataType: "jsonp",
+                jsonp: "c", // trigger MailChimp to return a JSONP response
+                contentType: "application/json; charset=utf-8",
+                error: function(error){
+                    // According to jquery docs, this is never called for cross-domain JSONP requests
+                },
+                success: function(data){
+                    if (data.result != "success") {
+                        var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
+                        if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+                            message = "You're already subscribed.";
+                            $message._hide();
+                            $message._show('failure', message);
+                        }
+                        $message._show('failure', message);
+                        $submit.disabled = false;
+                    } else {
+                              $message._show('success', 'Thank you!');
+                              $submit.disabled = false;
+                    }
+                }
+            });
+        }
+    });
